00ea54e83a0efa43197d05c56ac07cd3c3fb8d5f
Ali Ashraf <alyashrafelprince2004@gmail.com>
2024-08-23 22:22:01 +0300

    Finished login and Sign Up

--- app.py
+++ app.py
@@ -1,23 +1,63 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import db
-# import utils
+import utils
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 app = Flask(__name__, template_folder='src/templates', static_folder='src/static')
+connection = db.connect_to_database()
 app.secret_key = "SUPER-SECRET"
-# limiter = Limiter(app=app, key_func=get_remote_address, default_limits=["3 per minute"], storage_uri="memory://")
+limiter = Limiter(app=app, key_func=get_remote_address, default_limits=["50 per minute"], storage_uri="memory://")
 app.config['SESSION_COOKIE_HTTPONLY'] = False
 
-@app.route('/')
-def index():
-    return render_template('login.html')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['name']
+        password = request.form['password']
+        if not utils.is_strong_password(password):
+            flash("Sorry You Entered a weak Password Please Choose a stronger one", "danger")
+            return render_template('signup.html')
+
+        if db.get_user(connection, email):
+            flash("Email already exists. Please choose a different email.", "danger")
+            return render_template('signup.html')
+
+        if db.get_user_by_username(connection, username):
+            flash("Username already exists. Please choose a different username.", "danger")
+            return render_template('signup.html')
+        
+        db.add_user(connection, username, password, email)
+        return redirect(url_for('login'))
+
+    return render_template('signup.html')
 
 # @limiter.limit("3 per minute")
-# def login():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = db.get_user(connection, email)
+
+        if user:
+            if utils.is_password_match(password, user['password']):
+                session['username'] = user['username']
+                session['user_id'] = user['id']
+                return render_template('login.html')
+            else:
+                flash("Invalid Password or Email", "danger")
+                return flash("Invalid Password or Email", "danger")
+
+        else:
+            flash("Invalid Password or Email", "danger")
+            return flash("Invalid Password or Email", "danger")
+
+    return render_template('login.html')
 
 if __name__ == '__main__':
-    # db.init_db(connection)
-    # db.init_comments_table(connection)
-    # db.seed_admin_user(connection)
+    db.init_db(connection)
+    db.seed_admin_user(connection)
     app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,102 @@
+import utils
+
+
+def connect_to_database(name='database.db'):
+    import sqlite3
+    conn = sqlite3.connect(name, check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def init_db(connection):
+    cursor = connection.cursor()
+
+    cursor.execute('''
+		CREATE TABLE IF NOT EXISTS users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			username TEXT NOT NULL UNIQUE,
+			password TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            balance REAL NOT NULL DEFAULT 0,
+            credit_card TEXT 
+		);
+	''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS games (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            price REAL NOT NULL,
+            description TEXT NOT NULL,
+            genres TEXT NOT NULL,
+            release_date TEXT NOT NULL,
+            img_path TEXT NOT NULL
+        );
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS transactions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            paid_amount REAL NOT NULL,
+            date DATE DEFAULT CURRENT_DATE,
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE 
+        );
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS transaction_items (
+            transaction_id INTEGER NOT NULL,
+            game_id INTEGER NOT NULL, 
+            FOREIGN KEY (transaction_id) REFERENCES transactions(id) ON DELETE CASCADE, 
+            FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE 
+        );
+    ''')
+
+    connection.commit()
+
+
+def add_user(connection, username, password, email):
+    cursor = connection.cursor()
+    hashed_password = utils.hash_password(password)
+    query = '''INSERT INTO users (username, password, email) VALUES (?, ?, ?)'''
+    cursor.execute(query, (username, hashed_password, email))
+    connection.commit()
+
+
+def get_user(connection, email):
+    cursor = connection.cursor()
+    query = '''SELECT * FROM users WHERE email = ?'''
+    cursor.execute(query, (email,))
+    return cursor.fetchone()
+
+def get_user_by_username(connection, username):
+    cursor = connection.cursor()
+    query = '''SELECT * FROM users WHERE username = ?'''
+    cursor.execute(query, (username,))
+    return cursor.fetchone()
+
+
+def get_all_users(connection):
+    cursor = connection.cursor()
+    query = 'SELECT * FROM users'
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+def seed_admin_user(connection):
+    admin_email = 'admin@gmail.com'
+    admin_name = 'admin'
+    admin_password = 'admin'
+
+    # Check if admin user exists
+    admin_user = get_user(connection, admin_email)
+    if not admin_user:
+        add_user(connection, admin_name, admin_password, admin_email)
+
+
+def search_games(connection, search_query):
+    cursor = connection.cursor()
+    query = '''SELECT name FROM games WHERE name LIKE ?'''
+    cursor.execute(query, (f"%{search_query}%",))
+    return cursor.fetchall()
