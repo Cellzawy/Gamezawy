a0ede2580c7c928383a512a0527e26aaf779a4d0
Ali Ashraf <alyashrafelprince2004@gmail.com>
2024-08-23 23:05:01 +0300

    Mitigated sql injection, XSS

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import db
 import utils
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from markupsafe import Markup, escape
+
 
 app = Flask(__name__, template_folder='src/templates', static_folder='src/static')
 connection = db.connect_to_database()
@@ -13,20 +15,25 @@ app.config['SESSION_COOKIE_HTTPONLY'] = False
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        email = request.form['email']
-        username = request.form['name']
-        password = request.form['password']
+        email = escape(request.form['email'])
+        username = escape(request.form['name'])
+        password = escape(request.form['password'])
+        if not utils.is_email_valid(email) :
+            # flash("Invalid Email", "danger")
+            return "Invalid Email."
+
         if not utils.is_strong_password(password):
-            flash("Sorry You Entered a weak Password Please Choose a stronger one", "danger")
-            return render_template('signup.html')
+            # flash("Sorry You Entered a weak Password Please Choose a stronger one", "danger")
+            return "Sorry You Entered a weak Password Please Choose a stronger one"
+        
 
         if db.get_user(connection, email):
-            flash("Email already exists. Please choose a different email.", "danger")
-            return render_template('signup.html')
+            # flash("Email already exists. Please choose a different email.", "danger")
+            return "Email already exists."
 
         if db.get_user_by_username(connection, username):
-            flash("Username already exists. Please choose a different username.", "danger")
-            return render_template('signup.html')
+            # flash("Username already exists. Please choose a different username.", "danger")
+            return "Username already exists."
         
         db.add_user(connection, username, password, email)
         return redirect(url_for('login'))
@@ -40,20 +47,24 @@ def login():
         email = request.form['email']
         password = request.form['password']
 
+        if not utils.is_email_valid(email) :
+            # flash("Invalid Email", "danger")
+            return "Invalid Email."
+
         user = db.get_user(connection, email)
 
         if user:
             if utils.is_password_match(password, user['password']):
                 session['username'] = user['username']
                 session['user_id'] = user['id']
-                return render_template('login.html')
+                return "Logged in Successfully!"
             else:
-                flash("Invalid Password or Email", "danger")
-                return flash("Invalid Password or Email", "danger")
+                # flash("Invalid Password or Email", "danger")
+                return "Invalid Password or Email"
 
         else:
             flash("Invalid Password or Email", "danger")
-            return flash("Invalid Password or Email", "danger")
+            return "Invalid Password or Email", "danger"
 
     return render_template('login.html')
 
--- utils.py
+++ utils.py
@@ -38,3 +38,11 @@ def is_strong_password(password):
         return False
 
     return True
+
+def is_email_valid(email):
+
+    if(re.fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', email)):
+        return True
+
+    else:
+        return False
